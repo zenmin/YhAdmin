3f9b6ae8eeb4abc6502789fceaa3e30c39f18376
875784926@qq.com <lei.li@heweisoft.com>
2019-02-02 18:55:10 +0800

    提交ordersService

---
+++ src/main/java/com/yh/yhadmin/repository/OrdersRepository.java
@@ -0,0 +1,13 @@
+package com.yh.yhadmin.repository;
+
+import com.yh.yhadmin.domain.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Describle
+ * @Author Li lei
+ * @Date 2019/1/29 0029 15:08
+ * @Company
+ */
+public interface OrdersRepository extends JpaRepository<Orders,String>{
+}
