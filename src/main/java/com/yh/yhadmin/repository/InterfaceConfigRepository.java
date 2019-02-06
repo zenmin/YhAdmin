6385f8efccb01745cc5c71915ab54701ecbb2254
875784926@qq.com <lei.li@heweisoft.com>
2019-02-06 18:26:21 +0800

    提交repository

---
+++ src/main/java/com/yh/yhadmin/repository/InterfaceConfigRepository.java
@@ -0,0 +1,13 @@
+package com.yh.yhadmin.repository;
+
+import com.yh.yhadmin.domain.InterfaceConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Describle
+ * @Author Li lei
+ * @Date 2019/2/3 0003 15:57
+ * @Company
+ */
+public interface InterfaceConfigRepository extends JpaRepository<InterfaceConfig,String> {
+}
---
+++ src/main/java/com/yh/yhadmin/repository/WebConfigRepository.java
@@ -0,0 +1,13 @@
+package com.yh.yhadmin.repository;
+
+import com.yh.yhadmin.domain.WebConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Describle
+ * @Author Li lei
+ * @Date 2019/2/3 0003 15:58
+ * @Company
+ */
+public interface WebConfigRepository extends JpaRepository<WebConfig,String>{
+}
