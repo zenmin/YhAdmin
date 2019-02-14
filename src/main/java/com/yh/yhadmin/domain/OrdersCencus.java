e2e8768932ddb1a145704c8d1e7adcb213d9b76a
875784926@qq.com <lei.li@heweisoft.com>
2019-02-14 15:03:51 +0800

    新增统计信息实体类

---
+++ src/main/java/com/yh/yhadmin/domain/OrdersCencus.java
@@ -0,0 +1,24 @@
+package com.yh.yhadmin.domain;
+
+import lombok.Data;
+
+/**
+ * @Describle
+ * @Author Li lei
+ * @Date 2019/2/13 0013 17:28
+ * @Company
+ * 本类为统计订单所需
+ */
+@Data
+public class OrdersCencus {
+
+    private String total;//订单总数
+
+    private String complete;//交易完成
+
+    private String total_today;//今日总数
+
+    private String total_card;//卡密总数
+
+    private String Surplus;//剩余卡密
+}
